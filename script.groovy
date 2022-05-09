69b0f256a8987f1f25d986f416cf075cd120fda8
Himanshu Samal <39038939+Hsamal972@users.noreply.github.com>
2022-05-09 12:04:56 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,11 +1,3 @@
-def incrementVersion() {
-    echo 'incrementing the app version'
-    sh 'mvn build-helper:parse-version versions:set -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions:commit'
-    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
-    def version = matcher[0][1]
-    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
-}
-
 def commitVersionToGit() {
     echo 'commiting version to SCM'
     withCredentials([usernamePassword(credentialsId: 'github-username-password', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
@@ -18,9 +10,9 @@ def commitVersionToGit() {
 def dockerImage() {
     echo "Building the application"
     withCredentials([usernamePassword(credentialsId:'dockerhub-username-password',usernameVariable:'USER',passwordVariable:'PASS')]) {
-        sh "docker build -t doomedmonk13/test1:$IMAGE_NAME ."
+        sh "docker build -t doomedmonk13/test1:jma-${BUILD_NUMBER} ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push doomedmonk13/test1:$IMAGE_NAME"
+        sh "docker push doomedmonk13/test1:jma-${BUILD_NUMBER}"
     }
 }
 
