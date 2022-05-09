6619b5afc8f19b27057e2e8457a03a8b25697434
Himanshu Samal <39038939+Hsamal972@users.noreply.github.com>
2022-05-09 12:22:58 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,17 +1,17 @@
-def commitVersionToGit() {
-    echo 'commiting version to SCM'
-    withCredentials([usernamePassword(credentialsId: 'github-username-password', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'git add .'
-        sh 'git commit -m "push from jenkins"'
-        sh 'git push origin HEAD:jenkins-jobs'
-    }
-}
+// def commitVersionToGit() {
+//     echo 'commiting version to SCM'
+//     withCredentials([usernamePassword(credentialsId: 'github-username-password', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh 'git add .'
+//         sh 'git commit -m "push from jenkins"'
+//         sh 'git push origin HEAD:jenkins-jobs'
+//     }
+// }
 
 def dockerImage() {
     echo "Building the application"
-    withCredentials([usernamePassword(credentialsId:'dockerhub-username-password',usernameVariable:'USER',passwordVariable:'PASS')]) {
+    withCredentials([usernamePassword(credentialsId:'dockerhub-username-password',passwordVariable:'PASS', usernameVariable:'USER')]) {
         sh "docker build -t doomedmonk13/test1:jma-${BUILD_NUMBER} ."
-//         sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push doomedmonk13/test1:jma-${BUILD_NUMBER}"
     }
 }
