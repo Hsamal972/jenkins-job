06620f15b6fe67f77e3d1f1bb8fd7d308e1518df
Himanshu Samal <39038939+Hsamal972@users.noreply.github.com>
2022-05-10 10:40:29 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,7 +1,16 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn clean package -Dmaven.test.skip'
+}
+
+
 def dockerImage() {
-    echo "Building the application"
-    sh "docker build -t doomedmonk13/test1:jma-${BUILD_NUMBER} ."
-    sh "docker push doomedmonk13/test1:jma-${BUILD_NUMBER}"
+    echo "Building the application..."
+    withCredentials([usernamePassword(credentialsId:'dockerhub-username-password',usernameVariable:'USER',passwordVariable:'PASS')]) {
+        sh "docker build -t doomedmonk13/test1:$IMAGE_NAME ."
+        sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
+        sh "docker push doomedmonk13/test1:$IMAGE_NAME"
+    }
 }
 
 def deployApp() {
